54ff2ce6365f55a3ec14da408b8a8e3b3994807b
Baelul Haile <haile2baelul@gmail.com>
2020-07-06 17:08:13 +0000

    Comment section: Comments are stored in Datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -21,7 +24,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that handles comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
@@ -37,9 +40,16 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       String comment = request.getParameter("comment");
-      commentList.add(comment);
-      response.setContentType("text/html;");
-      response.getWriter().println(comment);
+      long timestamp = System.currentTimeMillis();
+
+      Entity commentEntity = new Entity("Comment");
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("timestamp", timestamp);
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+
+      response.sendRedirect("/comments.html");
   }
 
   private String convertToJson(ArrayList<String> commentList) {
